b4151dca46a74813ec122a183350c69afdb7f6fb
adjsky <igorlfmartins@mail.ru>
2022-04-02 13:49:50 +0300

    Don't refetch on focus

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,13 @@ import "@/styles/globals.css"
 
 import type { AppProps } from "next/app"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
